6f366255ff633aacd21b906bd0d0cec3bfb4fd8d
Bertrand Paquet <bpaquet@octo.com>
2012-11-08 21:41:41 +0100

    Update purge

--- purge.sh
+++ purge.sh
@@ -1,3 +1,5 @@
-find storage/  -name 'Package.*' | xargs rm -rf
-find storage/  -name 'Release.*' | xargs rm -rf
-find storage/  -name 'Source.*' | xargs rm -rf
+find storage/  -name 'Package*' | xargs rm -rf
+find storage/  -name 'Release*' | xargs rm -rf
+find storage/  -name 'Source*' | xargs rm -rf
+find storage/  -name '404' | xargs rm -rf
+find storage/  -name '500' | xargs rm -rf
